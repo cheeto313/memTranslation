1ecc2c3fd9c0e6785facb5574278258353c8f764
tatumd <devan.tatum@gmail.com>
2014-02-11 10:48:42 -0800

    Added file reading features yo.

--- memTrans.cpp
+++ memTrans.cpp
@@ -3,7 +3,40 @@
 
 #include <iostream>
 
-int main(){
-  std::cout << "Derp Derp Derp";
-  return 0;
+using std::string;
+using std::cout;
+using std::cin;
+//for file input-output
+using std:ifstream;
+
+int main(int argc, _TCHAR* arg[]){
+
+	bool inputOk = false;
+	string ext = ".txt";
+	string input;
+	string fOutput;
+
+	//input validation, make sure that it cannot sontinue without a file
+	//you spin me right round round right round...
+	while (!inputOk){
+	    cout << "Enter the name of text file (*.txt): ";
+	    cin >> input;
+	      
+	    if ((input.find(ext) != std::string::npos) ){
+	        inputOk = true;
+	    }
+	} 
+
+	ifstream fRead(input);
+
+	while(!fRead.eof()){
+		getline (fRead,fOutput);
+		cout << fOutput << std::endl;
+	}
+	//close the damn reader
+	fRead.close()
+
+	std::cout << "Derp Derp Derp";
+	return 0;
 }
+
