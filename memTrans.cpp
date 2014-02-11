74fc51a91b3d1103d3486bef47a219c175f022d8
tatumd <devan.tatum@gmail.com>
2014-02-11 11:23:37 -0800

    Finished reader/parser for input file.

--- memTrans.cpp
+++ memTrans.cpp
@@ -4,32 +4,33 @@
 #include <iostream>
 #include <fstream>
 
+//std stuff, because "using namspace std" is just lazy
 using std::string;
 using std::cout;
 using std::cin;
-//for file input-output
 using std::ifstream;
 
 int main(int argc, char* arg[]){
 
+	size_t pos = 0;
 	bool inputOk = false;
+	int count = 5;
 	string ext = ".txt";
 	string input;
 	string fOutput;
+	string temp;
 
 	//input validation, make sure that it cannot sontinue without a file
 	//you spin me right round round right round...
 	while (!inputOk){
-	    cout << "Enter the name of text file (*.txt): ";
+	    cout << "Enter the name of text file (*.txt) w/ the goods yo: ";
 	    cin >> input;
 	      
 	    if ((input.find(ext) != std::string::npos) ){
 	        inputOk = true;
 	    }
 	} 
-
 	ifstream fRead(input);
-
 	while(!fRead.eof()){
 		getline (fRead,fOutput);
 		cout << fOutput << std::endl;
@@ -37,7 +38,8 @@ int main(int argc, char* arg[]){
 	//close the damn reader
 	fRead.close();
 
-	std::cout << "Derp Derp Derp";
+	//herp
+	system("PAUSE");
 	return 0;
 }
 
